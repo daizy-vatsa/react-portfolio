649e2432333740a798bd5536896a303fa371a685
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-04-03 16:03:27 -0400

    Updates

---
+++ precache-manifest.952392df20a9036fe2b619867afac842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c97cca1566287dc313da8cf35d39657",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "58afab1ce6547f49a844",
+    "url": "/react-portfolio/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "ddcf13a8595b51b0ecf9",
+    "url": "/react-portfolio/static/js/2.85433851.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.85433851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58afab1ce6547f49a844",
+    "url": "/react-portfolio/static/js/main.2bd5174c.chunk.js"
+  },
+  {
+    "revision": "26950ce1f34a1b315732",
+    "url": "/react-portfolio/static/js/runtime-main.e44c21d0.js"
+  }
+]);
